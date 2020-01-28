12f0ae99eb428bb5107805ac2e05653c32abab0c
tothricsaj <tothricsaj@gmail.com>
2020-01-28 17:00:18 +0100

    Ttype block is yhea :bus:

--- src/services/BlockBuilder.js
+++ src/services/BlockBuilder.js
@@ -2,4 +2,21 @@ export class BlockBuilder {
     constructor() {
         this.a = 42;
     }
+
+    block(type, ctx) {
+        switch(type) {
+            case 'Ttype':
+                ctx.fillStyle = 'rgb(30, 140, 180)';
+                ctx.fillRect(30, 30, 10,10);
+                ctx.fillRect(40, 30, 10,10);
+                ctx.fillRect(50, 30, 10,10);
+                ctx.fillRect(40, 40, 10,10);
+
+                return 'Ttype';
+
+            default:
+                return 'No set type'
+
+        }
+    }
 }
--- src/services/GameRender.js
+++ src/services/GameRender.js
@@ -1,7 +1,10 @@
+import { BlockBuilder } from '../services/BlockBuilder.js';
+
 class GameRender {
     constructor() {
         this.canvas = null;
         this.ctx = null;
+        this.bb = new BlockBuilder();
     }
 
 
@@ -15,8 +18,10 @@ class GameRender {
     }
 
     testDraw() {
-        this.ctx.fillStyle = 'rgb(30, 140, 180)';
-        this.ctx.fillRect(30, 30, 40, 40);
+        // this.ctx.fillStyle = 'rgb(30, 140, 180)';
+        // this.ctx.fillRect(30, 30, 40, 40);
+        //
+        this.bb.block('Ttype', this.ctx);
     }
 }
 
