fd2a0de823d587cfa4e5e1a5ba9156cf56f63809
tothricsaj <tothricsaj@gmail.com>
2020-01-28 17:02:17 +0100

    No set type - default

--- src/__tests__/BlockBuilder.test.js
+++ src/__tests__/BlockBuilder.test.js
@@ -10,6 +10,7 @@ const bb = new BlockBuilder();
 describe('BlockBuilder features :)', () => {
     it('Blocks', () => {
         expect(bb.block('Ttype', ctx)).toEqual('Ttype');
+        expect(bb.block('falseType', ctx)).toEqual('No set type');
         expect(bb.block('Stype', ctx)).toEqual('Stype');
         expect(bb.block('Otype', ctx)).toEqual('Otype');
         expect(bb.block('Itype', ctx)).toEqual('Itype');
