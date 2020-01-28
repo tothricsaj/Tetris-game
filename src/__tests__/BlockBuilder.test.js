8efcc1a043137618874c4c5c816d92efcc3fce83
tothricsaj <tothricsaj@gmail.com>
2020-01-28 16:21:20 +0100

    BlockBuilder testing :duck:

---
+++ src/__tests__/BlockBuilder.test.js
@@ -0,0 +1,9 @@
+import { BlockBuilder } from '../services/BlockBuilder.js';
+
+const bb = new BlockBuilder();
+
+describe('BlockBuilder features :)', () => {
+    it('First', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
---
+++ src/services/BlockBuilder.js
@@ -0,0 +1,5 @@
+export class BlockBuilder {
+    constructor() {
+        this.a = 42;
+    }
+}
