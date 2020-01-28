c7ed01f88d91ed83dbadca4062c56ce433cdbcef
tothricsaj <tothricsaj@gmail.com>
2020-01-28 16:45:46 +0100

    canvas in jest problem is solved :smirk:

--- src/__tests__/BlockBuilder.test.js
+++ src/__tests__/BlockBuilder.test.js
@@ -1,10 +1,14 @@
+const { createCanvas } = require('canvas');
+const canvas = createCanvas(200, 200);
+const ctx = canvas.getContext('2d');
+// const BlockBuilder = require('../services/BlockBuilder.js');
+
 import { BlockBuilder } from '../services/BlockBuilder.js';
 
 const bb = new BlockBuilder();
 
 describe('BlockBuilder features :)', () => {
     it('Blocks', () => {
-        let ctx = document.getElementById('canvas').getContext('2d');
         expect(bb.block('Ttype', ctx)).toEqual('Ttype');
         expect(bb.block('Stype', ctx)).toEqual('Stype');
         expect(bb.block('Otype', ctx)).toEqual('Otype');
