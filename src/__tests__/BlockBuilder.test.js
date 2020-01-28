ac00246e8d17e7267c625e99b3efa537dc444810
tothricsaj <tothricsaj@gmail.com>
2020-01-28 16:31:49 +0100

    test of block types :tulip:

--- src/__tests__/BlockBuilder.test.js
+++ src/__tests__/BlockBuilder.test.js
@@ -3,7 +3,11 @@ import { BlockBuilder } from '../services/BlockBuilder.js';
 const bb = new BlockBuilder();
 
 describe('BlockBuilder features :)', () => {
-    it('First', () => {
-        expect(1 + 1).toBe(2);
+    it('Blocks', () => {
+        let ctx = document.getElementById('canvas').getContext('2d');
+        expect(bb.block('Ttype', ctx)).toEqual('Ttype');
+        expect(bb.block('Stype', ctx)).toEqual('Stype');
+        expect(bb.block('Otype', ctx)).toEqual('Otype');
+        expect(bb.block('Itype', ctx)).toEqual('Itype');
     });
 });
