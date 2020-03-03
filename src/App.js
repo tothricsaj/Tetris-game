db7c60d226753013603bd602f419ed6ba871f81e
tothricsaj <tothricsaj@gmail.com>
2020-03-03 13:47:20 +0100

    notes before refactor merge

--- src/App.js
+++ src/App.js
@@ -18,3 +18,5 @@ export default App;
 // So, this is ownig to gitflow
 // block controll is working nonetheless I have begun to develop the gameplace
 // OK, gameover if the block on the top
+// Before refactor merge. It is not too much nonetheless personally I beleive that
+// the refactor makes the life by far esier than I just still mess of code.
